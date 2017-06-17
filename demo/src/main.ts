0bd8a60c39a979e60fb2d92db785be6e32f0cb52
Jay Kan <leicasper@gmail.com>
2017-06-16 23:22:16 -0700

    example: add components task 3

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -9,7 +9,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-console.log('Angular4 Components (Task 2)');
+console.log('Angular4 Components (Task 3)');
 
 export function main() {
   platformBrowserDynamic()
