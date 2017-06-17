580215089a89d55ca6509afadd91da93e1af8123
Jay Kan <leicasper@gmail.com>
2017-06-16 21:39:55 -0700

    example: add Angular4 Basics task 1

--- app/interfaces.ts
+++
@@ -1,8 +0,0 @@
-export interface IProduct {
-  name: string;
-  description: string;
-  price: number;
-  promoted?: boolean;
-}
-
-export type CustomElement = string | { tagName: string, children: CustomElement[] };
---
+++ demo/src/main.ts
@@ -0,0 +1,24 @@
+import './polyfills.ts';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+console.log('Angular4 Basics 1');
+
+export function main() {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch(error => console.error(error));
+}
+
+if (document.readyState === 'complete') {
+  main();
+} else {
+  document.addEventListener('DOMContentLoaded', main);
+}
